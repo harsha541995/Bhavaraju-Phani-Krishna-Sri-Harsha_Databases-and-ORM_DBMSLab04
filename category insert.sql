0217a5242ca0e2926d3aa46ede01c04b116b9c37
harsha541995 <116940389+harsha541995@users.noreply.github.com>
2023-02-28 21:18:16 +0530

    Delete category insert.sql

--- category insert.sql
+++
@@ -1,5 +0,0 @@
-insert into category values(1,"BOOKS");
-insert into category values(2,"GAMES");
-insert into category values(3,"GROCERIES");
-insert into category values(4,"ELECTRONICS");
-insert into category values(5,"CLOTHES");
